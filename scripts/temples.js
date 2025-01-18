77f5b533928a2ffdaf334ce9994404be27551308
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-01-17 22:01:28 -0300

    layout done and js added

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,3 +2,10 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent =
   "Last modified: " + document.lastModified;
+
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+
+menuIcon.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
